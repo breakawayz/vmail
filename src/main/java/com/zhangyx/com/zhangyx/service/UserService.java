675ab475e1d437a85b0dd98f703562310b2e5f58
zhangyx <zhangyx@firstshare.cn>
2017-09-03 19:30:03 +0800

    首次上传

---
+++ src/main/java/com/zhangyx/com/zhangyx/service/UserService.java
@@ -0,0 +1,11 @@
+package com.zhangyx.com.zhangyx.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserService {
+
+    public String queryName(){
+        return "yes hello";
+    }
+}
